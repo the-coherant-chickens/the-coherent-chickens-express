ec21b97891fb5d172905e63d66662f48fad07001
JHankins09 <Hankins.JamesB@gmail.com>
2019-08-09 14:54:32 -0400

    Apply logic to update tags as array

--- app/routes/image_routes.js
+++ app/routes/image_routes.js
@@ -94,13 +94,23 @@ router.patch('/images/:id', requireToken, removeBlanks, (req, res, next) => {
   delete req.body.image.owner
 
   ImageUpload.findById(req.params.id)
-    .populate('owner')
     .then(handle404)
     .then(image => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
       requireOwnership(req, image)
-
+      console.log(req.body.image.tag)
+      if (req.body.image.tag === true) {
+        let arrayToUpdate = []
+        let i = 0
+        while (i < req.body.image.tag.split(',').length) {
+          const tagsToPush = req.body.image.tag.split(',')
+          arrayToUpdate.push(tagsToPush[i].trim())
+          i += 1
+        }
+        req.body.image.tag = arrayToUpdate
+      }
+      console.log('post array-ify', req.body.image.tag)
       // pass the result of Mongoose's `.update` to the next `.then`
       return image.update(req.body.image)
     })
